125c970cec161c7630a0c2cbff407c9dad913f8f
LynxVito <mzb508@york.ac.uk>
2023-10-05 18:07:35 +0100

    bouncing + random

--- sketch.js
+++ sketch.js
@@ -1,12 +1,29 @@
 const GRID_SIZE = 20;
-let headX = 0
-let headY = 60
-let foodX = 140
-let foodY = 60
+let headX = 0;
+let headY = 60;
+let foodX;
+let foodY;
+let numberOfCellsX;
+let numberOfCellsY;
+movingRight = true;
+let randomCellX;
+let randomCellFloorX;
+let randomCellY;
+let randomCellFloorY;
 
 function setup() {
     createCanvas(600, 600)
     frameRate(3)
+
+    numberOfCellsX = width / GRID_SIZE
+    randomCellX = random(numberOfCellsX)
+    randomCellFloorX = floor(randomCellX)
+    foodX = randomCellFloorX * GRID_SIZE + GRID_SIZE * 2
+
+    numberOfCellsY = height / GRID_SIZE
+    randomCellY = random(numberOfCellsY)
+    randomCellFloorY = floor(randomCellY)
+    foodY = randomCellFloorY * GRID_SIZE + GRID_SIZE * 2
 }
 function draw() {
     background(255)
@@ -15,6 +32,17 @@ function draw() {
     fill(0, 255, 0)
     square(headX, headY, GRID_SIZE)
     fill(255, 180, 50)
-    circle(140, 60, GRID_SIZE)
-    headX = headX + GRID_SIZE
+    circle(foodX, foodY, GRID_SIZE)
+    if(!movingRight && headX <= 0) {
+        movingRight = true;
+    }
+    else if(movingRight && headX < width) {
+        headX += GRID_SIZE;
+    }
+    else if(movingRight && headX >= width) {
+        movingRight = false;
+    }
+    else if(!movingRight && headX > 0) {
+        headX -= GRID_SIZE;
+    }
 }
