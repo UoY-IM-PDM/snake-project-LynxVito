36f4d2bb6255dd38af58787619b91c908f58edcf
LynxVito <mzb508@york.ac.uk>
2023-10-17 16:09:31 +0100

    did stuff

--- sketch.js
+++ sketch.js
@@ -9,103 +9,87 @@ let headY;
 
 function setup() {
     createCanvas(600, 600);
-    frameRate(5);
-
-    numberOfCellsX = width / GRID_SIZE;
-    randomCellX = random(numberOfCellsX);
-    randomCellFloorX = floor(randomCellX);
-    foodX = randomCellFloorX * GRID_SIZE + GRID_SIZE * 2;
-
-    numberOfCellsY = height / GRID_SIZE;
-    randomCellY = random(numberOfCellsY);
-    randomCellFloorY = floor(randomCellY);
-    foodY = randomCellFloorY * GRID_SIZE + GRID_SIZE * 2;
+    frameRate(3);
 
     speedX = GRID_SIZE
+    speedY = 0
 
-    food1X = random(0, width);
-    food1Y = random(0, width);
-    food2X = random(0, width);
-    food2Y = random(0, width);
-    food3X = random(0, width);
-    food3Y = random(0, width);
+    headX = GRID_SIZE
+    headY = GRID_SIZE
+
+    food1X = newFoodCoordinate();
+    food1Y = newFoodCoordinate();
+    food2X = newFoodCoordinate();
+    food2Y = newFoodCoordinate();
+    food3X = newFoodCoordinate();
+    food3Y = newFoodCoordinate();
 
 }
 
 function draw() {
     background(255)
-    headX = constrain(speedX, GRID_SIZE, width - GRID_SIZE);
-    headY = constrain(speedY, GRID_SIZE, height - GRID_SIZE);
-    foodX = constrain(foodX, GRID_SIZE, width - GRID_SIZE);
-    foodY = constrain(foodY, GRID_SIZE, height - GRID_SIZE);
     noStroke();
     rectMode(CENTER);
-
-    if(headX > width) {
-        speedX = -speedX;
-    }
-
-    if(headX < 0) {
-        speedX = speedX * -1;
-    }
-
-    fill(0, 255, 0);
-    square(headX, GRID_SIZE, GRID_SIZE);
 
     food(food1X, food1Y);
     food(food2X, food2Y);
     food(food3X, food3Y);
 
-    /* if(speedX > 0) {
-        headX += GRID_SIZE;
-    }
-    else if(speedX < 0) {
-        headX += -GRID_SIZE;
-    }
-    else {
-        headX = 0;
-    }
+    headX += speedX;
+    headY += speedY;
 
+    headX = constrain(headX, GRID_SIZE, width - GRID_SIZE);
+    headY = constrain(headY, GRID_SIZE, height - GRID_SIZE);
 
-    if(speedY > 0) {
-        headY += GRID_SIZE;
-    }
-    else if(speedY < 0) {
-        headY += -GRID_SIZE;
-    }
-    else {
-        headY = 0;
-    } */
+    fill(0, 255, 0);
+    square(headX, headY, GRID_SIZE);
+
+    // eatsFood() 
 
 }
 
  function food(foodX, foodY) {
+    foodX = constrain(foodX, GRID_SIZE, width - GRID_SIZE);
+    foodY = constrain(foodY, GRID_SIZE, height - GRID_SIZE);
+
     fill(255, 180, 50);
     circle(foodX, foodY, GRID_SIZE);
 } 
 
 function keyPressed() {
     if(key === "a") {
-        speedX = -speedX;
+        speedX = -GRID_SIZE;
+        speedY = 0;
     }
     else if(key === "d") {
-        speedX = speedX;
+        speedX = GRID_SIZE;
+        speedY = 0;
     }
-    else {
+
+    else if(key === "w") {
+        speedY = -GRID_SIZE;
+        speedX = 0
+    } 
+    else if(key === "s") {
+        speedY = GRID_SIZE;
         speedX = 0
     }
-
-    if(key === "w") {
-        speedY = speedY;
-    }
-    else if(key === "s") {
-        speedY = -speedY;
-    }
-    else {
-        speedY = 0
-    }
 }
 
-function head(headX, headY) {
-    
+function newFoodCoordinate() {
+    numberOfCellsX = width / GRID_SIZE;
+    randomCellX = random(numberOfCellsX);
+    randomCellFloorX = floor(randomCellX);
+    return randomCellFloorX * GRID_SIZE + GRID_SIZE * 2;
 }
+
+/* function eatsFood() {
+    if(headX === food1X && headY === food1Y()) {
+        food1X = newFoodCoordinate();
+        food1Y = newFoodCoordinate();
+        food2X = newFoodCoordinate();
+        food2Y = newFoodCoordinate();
+        food3X = newFoodCoordinate();
+        food3Y = newFoodCoordinate();
+    }
+} */
